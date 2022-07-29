774d0b1e9a54381ac2b3a312f013065f3ee2cc01
ahyaemon <iamahyaemon@gmail.com>
2022-07-29 09:20:20 +0900

    other page に tet simulator を追加

--- src/pages/OthersPage.tsx
+++ src/pages/OthersPage.tsx
@@ -8,6 +8,7 @@ export const OthersPage: Component = () => {
 
             <h2>他</h2>
             <p><a href="https://github.com/ahyaemon/official_web_site" target="_blank">github</a></p>
+            <p><a href="https://tet-simulator.ahyaemon.com/" target="_blank">テトリスシミュレーター</a></p>
             <p>虫食い算やろうぜ！！というサイトを昔運営してたけど公開停止した</p>
         </div>
     )
